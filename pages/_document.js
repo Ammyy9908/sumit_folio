1f15bda4173db13abf69d95e244918dd5fa46bab
Sumit <sumit@kodnest.com>
2023-02-14 11:50:01 +0530

    clarity fixed

--- pages/_document.js
+++ pages/_document.js
@@ -22,15 +22,17 @@ export default function Document() {
           async
         ></script>
 
-<script type="text/javascript" async>
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "ftyp3rae24");
-
-
-</script>
+        <script type="text/javascript" async>
+          dangerouslySetInnerHTML=
+          {{
+            __html: `
+     (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+  })(window, document, "clarity", "script", "ftyp3rae24");`,
+          }}
+        </script>
       </body>
     </Html>
   );
